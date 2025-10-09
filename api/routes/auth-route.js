c18c00c7c43e9d0b8cf3a7bcc6abc8507cf8cd73
Enock Yator <ekyator02@gmail.com>
2025-10-09 11:52:53 +0300

    add check-auth route with GET request

--- api/routes/auth-route.js
+++ api/routes/auth-route.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const express = require('express');
-const { registerUser, loginUser, refreshRefreshToken, logoutUser } = require('../services/auth-service');
+const { registerUser, loginUser, refreshRefreshToken, logoutUser, checkAuth } = require('../services/auth-service');
 
 // create router interface
 const router = express.Router();
@@ -11,5 +11,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/refresh", refreshRefreshToken); // uses cookie
 router.post("/logout", logoutUser);
+router.get("check-auth", checkAuth);
+
 
 module.exports = router;
